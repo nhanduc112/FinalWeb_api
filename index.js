e43915d6a392ddf42e2dd8140f7df38ef2847ee9
nhanduc112 <115692156+nhanduc112@users.noreply.github.com>
2023-12-18 23:39:28 +0700

    commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
-import hotelsRoute from "./routes/auth.js";
-import roomsRoute from "./routes/auth.js";
-import usersRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
 
 const app = express();
 dotenv.config();
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,61 +1,31 @@
 import express from "express";
+import {
+    
+    createHotel,
+    deleteHotel,
+    getHotel,
+    getHotels,
+    updateHotel,
+  } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
-//Create
-router.post("/", async (req, res) => { 
-    const newHotel = new Hotel(req.body);
+//CREATE
+router.post("/", createHotel);
 
-    try { 
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    
-    } catch (err) { 
-        res.status(500).json(err);
-    }
-});
+//UPDATE
+router.put("/:id", updateHotel);
 
-//Update
-router.put("/:id", async (req, res) => { 
-    try { 
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
-        res.status(200).json(updatedHotel);
-    
-    } catch (err) { 
-        res.status(500).json(err);
-    }
-});
+//DELETE
+router.delete("/:id", deleteHotel);
 
-//Delete
-router.delete("/:id", async (req, res) => { 
-    try { 
-        await Hotel.findByIdAndDelete(req.params.id,);
-        res.status(200).json("Hoel has been deleted.");
-    } catch (err) { 
-        res.status(500).json(err);
-    }
-});
+//GET
+router.get("/find/:id", getHotel);
 
-//Get
-router.get("/:id", async (req, res) => { 
-    try { 
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-    
-    } catch (err) { 
-        res.status(500).json(err);
-    }
-});
+//GET ALL
+router.get("/", getHotels);
 
-//Get all
-router.put("/", async (req, res) => { 
-    try { 
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    
-    } catch (err) { 
-        res.status(500).json(err);
-    }
-});
+
 
 export default router
